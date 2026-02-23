f5c33ba8c4a9ca272f090d0e13717c935481e799
Giovanni <smith.alvarez.g@gmail.com>
2026-02-23 15:17:55 -0500

    Navi Setup

--- .config/fish/functions/j.fish
+++ .config/fish/functions/j.fish
@@ -1,8 +1,7 @@
 function j
     if test (count $argv) -eq 0
         # Get current dir and strip any trailing slash for a clean match
-        set -l current_dir (string trim --suffix / (pwd))
-
+        set -l current_dir (pwd | string replace -r '/$' '')
         # Use a regex anchor to ensure we match exactly the path or the path with a slash
         set -l drive (zoxide query -l | string match -v -r "^$current_dir/?\$" | fzf --height 40% --layout=reverse --border --preview 'eza -T -L 1 --icons --color=always {1}')
 
